8e0c417ce76a17ffb25296af336b9b3c3b53fe2f
Sebastian B <seb@bgaard.dk>
2018-03-21 13:08:35 +0100

    Gradle setup (#1)
    * update gitignore
    
    * gradle setup complete
    
    * Add Test to test Gradle
    
    * Add Lib jar file

---
+++ src/test/java/GradleTest.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class GradleTest{
+    
+    @Test
+    public void TestSimple() {
+        assertTrue(1 == 1);
+    }
+}
